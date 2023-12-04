1c3179e4a879ea065603dd67fe50d78739b2281e
tim <tim@timschupp.de>
2023-12-04 06:14:50 +0100

    add atoms and further configure tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: [
     "./renderer/**/*.{vue,js,ts,jsx,tsx}",
     "./pages/**/*.{vue,js,ts,jsx,tsx}",
+    "./atoms/**/*.{vue,js,ts,jsx,tsx}",
+    'node_modules/daisyui/dist/**/*.js',
+    'node_modules/react-daisyui/dist/**/*.js',
   ],
   theme: {
     extend: {},
