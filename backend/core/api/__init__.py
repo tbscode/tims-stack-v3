907f51c637a875a182ae36be4681b6ff41916ee1
tim <tim@timschupp.de>
2023-12-04 06:25:21 +0100

    full reset

--- backend/core/api/__init__.py
+++
@@ -1,6 +0,0 @@
-from .register import register_user
-from .user_data import request_user_data
-from .login import login_user
-from .profile import UpdateProfileViewset
-from .chats import ChatsModelViewSet
-from .messages import MessagesModelViewSet
--- frontend/renderer/store/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import { messagesReducer } from './messagesReducer';
-import { chatsReducer } from './chatsReducer';
-import { userReducer } from './userReducer';
-export const rootReducer = combineReducers({
-    messages: messagesReducer,
-    chats: chatsReducer,
-    user: userReducer,
-});
