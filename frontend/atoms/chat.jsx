13b7743b7afc49b9b9cb053e38ebf555705e97af
tim <tim@timschupp.de>
2024-01-29 19:23:07 +0100

    improve formatting

--- frontend/atoms/chat.jsx
+++ frontend/atoms/chat.jsx
@@ -1,35 +1,39 @@
-export function ChatItem({
-    partnerImage,
-    chatText,
-    onSelected,
-    uuid
-}){
-    return <button key={uuid} className="btn" onClick={() => {
-      onSelected();
-    }}>
-  {chatText}
-</button>
+import React from "react";
+
+export function ChatItem({ partnerImage, chatText, onSelected, uuid }) {
+  return (
+    <button
+      key={uuid}
+      className="btn"
+      onClick={() => {
+        onSelected();
+      }}
+    >
+      {chatText}
+    </button>
+  );
 }
 
-export function ChatItemSkelleton({}){
-    return <button className="btn skeleton">
+export function ChatItemSkelleton({}) {
+  return (
+    <button className="btn skeleton">
       <div className="chat-image avatar">
-        <div className="w-10 rounded-full">
-        </div>
-    </div>
-</button>
+        <div className="w-10 rounded-full"></div>
+      </div>
+    </button>
+  );
 }
 
 export function ChatMessage({
-    senderName,
-    senderImage,
-    isSelf,
-    message,
-    timeSend,
-    timeSeen
+  senderName,
+  senderImage,
+  isSelf,
+  message,
+  timeSend,
+  timeSeen,
 }) {
-
-   return <div className={`chat chat-${isSelf ? 'end' : 'start'}`}>
+  return (
+    <div className={`chat chat-${isSelf ? "end" : "start"}`}>
       <div className="chat-image avatar">
         <div className="w-10 rounded-full">
           <img alt="Tailwind CSS chat bubble component" src={senderImage} />
@@ -40,24 +44,17 @@ export function ChatMessage({
         <time className="text-xs opacity-50">{timeSend}</time>
       </div>
       <div className="chat-bubble">{message}</div>
-      {timeSeen && <div className="chat-footer opacity-50">
-        {timeSeen}
-      </div>}
-      {!timeSeen && <div className="chat-footer opacity-50">
-        Sending... 
-      </div>}
+      {timeSeen && <div className="chat-footer opacity-50">{timeSeen}</div>}
+      {!timeSeen && <div className="chat-footer opacity-50">Sending...</div>}
     </div>
+  );
 }
 
-export function MessageSkelleton({
-    isSelf,
-    id
-}) {
-
-   return <div key={id} className={`chat chat-${isSelf ? 'end' : 'start'}`}>
+export function MessageSkelleton({ isSelf, id }) {
+  return (
+    <div key={id} className={`chat chat-${isSelf ? "end" : "start"}`}>
       <div className="skeleton chat-image avatar">
-        <div className="w-10 rounded-full">
-        </div>
+        <div className="w-10 rounded-full"></div>
       </div>
       <div className="skeleton chat-header">
         <time className="text-xs opacity-50"></time>
@@ -65,9 +62,8 @@ export function MessageSkelleton({
       <div className="skeleton chat-bubble">
         {"                                   "}
       </div>
-      <div className="chat-footer opacity-50">
-      </div>
-      <div className="chat-footer opacity-50">
-      </div>
+      <div className="chat-footer opacity-50"></div>
+      <div className="chat-footer opacity-50"></div>
     </div>
+  );
 }
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,55 +1,63 @@
-import react from '@vitejs/plugin-react'
-import tailwindcss from 'tailwindcss';
-import vike from 'vike/plugin'
+import react from "@vitejs/plugin-react";
+import tailwindcss from "tailwindcss";
+import vike from "vike/plugin";
 import rollupNodePolyFill from "rollup-plugin-node-polyfills";
-import { builtinModules } from 'module';
-
+import { builtinModules } from "module";
 
 export default {
-  plugins: [react(), vike({
-    prerender: true
-  }), tailwindcss()],
+  plugins: [
+    react(),
+    vike({
+      prerender: true,
+    }),
+    tailwindcss(),
+  ],
   alias: {
     resolve: {
-      './runtimeConfig': './runtimeConfig.browser',
-    }
+      "./runtimeConfig": "./runtimeConfig.browser",
+    },
   },
   build: {
     rollupOptions: {
-      external: [...builtinModules, /^node:/]
-    }
+      external: [...builtinModules, /^node:/],
+    },
   },
   server: {
     proxy: {
-      '/admin': {
-           target: 'http://host.docker.internal:8000',
-           changeOrigin: true,
-           secure: false,      
-           ws: true,
-       },
-      '/static': {
-           target: 'http://host.docker.internal:8000',
-           changeOrigin: true,
-           secure: false,      
-           ws: true,
-       },
-      '/api': {
-           target: 'http://host.docker.internal:8000',
-           changeOrigin: true,
-           secure: false,      
-           ws: true,
-           configure: (proxy, _options) => {
-            proxy.on('error', (err, _req, _res) => {
-              console.log('proxy error', err);
-            });
-            proxy.on('proxyReq', (proxyReq, req, _res) => {
-              console.log('Sending Request to the Target:', req.method, req.url);
-            });
-            proxy.on('proxyRes', (proxyRes, req, _res) => {
-              console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
-            });
-          },
-       }
-    }
-  }
-}
+      "/admin": {
+        target: "http://host.docker.internal:8000",
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+      },
+      "/static": {
+        target: "http://host.docker.internal:8000",
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+      },
+      "/api": {
+        target: "http://host.docker.internal:8000",
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+        configure: (proxy, _options) => {
+          proxy.on("error", (err, _req, _res) => {
+            console.log("proxy error", err);
+          });
+          proxy.on("proxyReq", (proxyReq, req, _res) => {
+            console.log(proxyReq, req);
+            console.log("Sending Request to the Target:", req.method, req.url);
+          });
+          proxy.on("proxyRes", (proxyRes, req, _res) => {
+            console.log(
+              "Received Response from the Target:",
+              proxyRes.statusCode,
+              req.url
+            );
+          });
+        },
+      },
+    },
+  },
+};
