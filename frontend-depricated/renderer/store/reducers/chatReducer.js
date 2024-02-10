22e0b56950056cce0b0aed3a1cfaf42de2104d20
tim <tim@timschupp.de>
2024-02-10 14:07:10 +0100

    deleted old fronteend

--- frontend-depricated/renderer/store/reducers/chatReducer.js
+++
@@ -1,8 +0,0 @@
-export function chatReducer(state = null, action) {
-  switch (action.type) {
-    case "initChat":
-      return { ...state, ...action.payload };
-    default:
-      return state;
-  }
-}
